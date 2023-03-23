106a38db12fe4b86ac03951742eee8030d3f99e5
Guilherme Mateus Sousa Santos <domiuaunoob@outlook.com>
2023-03-23 14:12:59 -0300

    Estado atual: O jogo já está funcional, mas as cartas especiais (+2, +4, inverter, bloquear e espelho) ainda não estão funcionando corretamente.

---
+++ src/B.java
@@ -0,0 +1,24 @@
+public class B {
+
+    public static void main(String[] args) {
+
+        String a = "\u001B[31m┌─────────────────┐\n" +
+                "\u001B[31m│                 │\n" +
+                "\u001B[31m│     ┌────/┐     │\n" +
+                "\u001B[31m│     │   / │     │\n" +
+                "\u001B[31m│     │  /  │     │\n" +
+                "\u001B[31m│     │ /   │     │\n" +
+                "\u001B[31m│     └/────┘     │\n" +
+                "\u001B[31m│                 │\n" +
+                "\u001B[31m│   VERMELHO #B   │\n" +
+                "\u001B[31m│                 │\n" +
+                "\u001B[31m└─────────────────┘";
+
+        System.out.println(a.substring(0,24));
+
+    }
+
+
+
+
+}
